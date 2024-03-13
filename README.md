6a32061595308d8d31ea66f3819d4fafa3c59273
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2024-03-13 14:31:39 -0400

    cli initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # uniGradICON
 The official website for uniGradICON: A Foundation Model for Medical Image Registration
+
+[paper](https://arxiv.org/abs/2403.05780)
+
+```
+@misc{tian2024unigradicon,
+      title={uniGradICON: A Foundation Model for Medical Image Registration}, 
+      author={Lin Tian and Hastings Greer and Roland Kwitt and Francois-Xavier Vialard and Raul San Jose Estepar and Sylvain Bouix and Richard Rushmore and Marc Niethammer},
+      year={2024},
+      eprint={2403.05780},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV}
+}
+```
+
+
+To use:
+
+```
+pip install unigradicon
+
+wget http://www.slicer.org/w/img_auth.php/5/59/RegLib_C01_1.nrrd
+wget http://www.slicer.org/w/img_auth.php/e/e3/RegLib_C01_2.nrrd
+
+unigradicon-register --fixed=RegLib_C01_2.nrrd --moving=RegLib_C01_1.nrrd --transform_out=trans.hdf5 --warped_moving_out=warped_C01_1.nrrd
+
+```
+
+The result can be viewed in Slicer
+![result](slicer_output.png?raw=true)
---
+++ src/unigradicon.py
@@ -0,0 +1,221 @@
+import itk
+import os
+from datetime import datetime
+
+import footsteps
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+import icon_registration as icon
+import icon_registration.network_wrappers as network_wrappers
+import icon_registration.networks as networks
+from icon_registration import config
+from icon_registration.losses import ICONLoss, to_floats
+from icon_registration.mermaidlite import compute_warped_image_multiNC
+import icon_registration.itk_wrapper
+
+
+
+input_shape = [1, 1, 175, 175, 175]
+
+class GradientICONSparse(network_wrappers.RegistrationModule):
+    def __init__(self, network, similarity, lmbda):
+
+        super().__init__()
+
+        self.regis_net = network
+        self.lmbda = lmbda
+        self.similarity = similarity
+
+    def forward(self, image_A, image_B):
+
+        assert self.identity_map.shape[2:] == image_A.shape[2:]
+        assert self.identity_map.shape[2:] == image_B.shape[2:]
+
+        # Tag used elsewhere for optimization.
+        # Must be set at beginning of forward b/c not preserved by .cuda() etc
+        self.identity_map.isIdentity = True
+
+        self.phi_AB = self.regis_net(image_A, image_B)
+        self.phi_BA = self.regis_net(image_B, image_A)
+
+        self.phi_AB_vectorfield = self.phi_AB(self.identity_map)
+        self.phi_BA_vectorfield = self.phi_BA(self.identity_map)
+
+        # tag images during warping so that the similarity measure
+        # can use information about whether a sample is interpolated
+        # or extrapolated
+
+        if getattr(self.similarity, "isInterpolated", False):
+            # tag images during warping so that the similarity measure
+            # can use information about whether a sample is interpolated
+            # or extrapolated
+            inbounds_tag = torch.zeros([image_A.shape[0]] + [1] + list(image_A.shape[2:]), device=image_A.device)
+            if len(self.input_shape) - 2 == 3:
+                inbounds_tag[:, :, 1:-1, 1:-1, 1:-1] = 1.0
+            elif len(self.input_shape) - 2 == 2:
+                inbounds_tag[:, :, 1:-1, 1:-1] = 1.0
+            else:
+                inbounds_tag[:, :, 1:-1] = 1.0
+        else:
+            inbounds_tag = None
+
+        self.warped_image_A = compute_warped_image_multiNC(
+            torch.cat([image_A, inbounds_tag], axis=1) if inbounds_tag is not None else image_A,
+            self.phi_AB_vectorfield,
+            self.spacing,
+            1,
+            zero_boundary=True
+        )
+        self.warped_image_B = compute_warped_image_multiNC(
+            torch.cat([image_B, inbounds_tag], axis=1) if inbounds_tag is not None else image_B,
+            self.phi_BA_vectorfield,
+            self.spacing,
+            1,
+            zero_boundary=True
+        )
+
+        similarity_loss = self.similarity(
+            self.warped_image_A, image_B
+        ) + self.similarity(self.warped_image_B, image_A)
+
+        if len(self.input_shape) - 2 == 3:
+            Iepsilon = (
+                self.identity_map
+                + 2 * torch.randn(*self.identity_map.shape).to(config.device)
+                / self.identity_map.shape[-1]
+            )[:, :, ::2, ::2, ::2]
+        elif len(self.input_shape) - 2 == 2:
+            Iepsilon = (
+                self.identity_map
+                + 2 * torch.randn(*self.identity_map.shape).to(config.device)
+                / self.identity_map.shape[-1]
+            )[:, :, ::2, ::2]
+
+        # compute squared Frobenius of Jacobian of icon error
+
+        direction_losses = []
+
+        approximate_Iepsilon = self.phi_AB(self.phi_BA(Iepsilon))
+
+        inverse_consistency_error = Iepsilon - approximate_Iepsilon
+
+        delta = 0.001
+
+        if len(self.identity_map.shape) == 4:
+            dx = torch.tensor([[[[delta]], [[0.0]]]]).to(config.device)
+            dy = torch.tensor([[[[0.0]], [[delta]]]]).to(config.device)
+            direction_vectors = (dx, dy)
+
+        elif len(self.identity_map.shape) == 5:
+            dx = torch.tensor([[[[[delta]]], [[[0.0]]], [[[0.0]]]]]).to(config.device)
+            dy = torch.tensor([[[[[0.0]]], [[[delta]]], [[[0.0]]]]]).to(config.device)
+            dz = torch.tensor([[[[0.0]]], [[[0.0]]], [[[delta]]]]).to(config.device)
+            direction_vectors = (dx, dy, dz)
+        elif len(self.identity_map.shape) == 3:
+            dx = torch.tensor([[[delta]]]).to(config.device)
+            direction_vectors = (dx,)
+
+        for d in direction_vectors:
+            approximate_Iepsilon_d = self.phi_AB(self.phi_BA(Iepsilon + d))
+            inverse_consistency_error_d = Iepsilon + d - approximate_Iepsilon_d
+            grad_d_icon_error = (
+                inverse_consistency_error - inverse_consistency_error_d
+            ) / delta
+            direction_losses.append(torch.mean(grad_d_icon_error**2))
+
+        inverse_consistency_loss = sum(direction_losses)
+
+        all_loss = self.lmbda * inverse_consistency_loss + similarity_loss
+
+        transform_magnitude = torch.mean(
+            (self.identity_map - self.phi_AB_vectorfield) ** 2
+        )
+        return icon.losses.ICONLoss(
+            all_loss,
+            inverse_consistency_loss,
+            similarity_loss,
+            transform_magnitude,
+            icon.losses.flips(self.phi_BA_vectorfield),
+        )
+
+    def clean(self):
+        del self.phi_AB, self.phi_BA, self.phi_AB_vectorfield, self.phi_BA_vectorfield, self.warped_image_A, self.warped_image_B
+
+def make_network(input_shape, include_last_step=False, lmbda=1.5, loss_fn=icon.LNCC(sigma=5)):
+    dimension = len(input_shape) - 2
+    inner_net = icon.FunctionFromVectorField(networks.tallUNet2(dimension=dimension))
+
+    for _ in range(2):
+        inner_net = icon.TwoStepRegistration(
+            icon.DownsampleRegistration(inner_net, dimension=dimension),
+            icon.FunctionFromVectorField(networks.tallUNet2(dimension=dimension))
+        )
+    if include_last_step:
+        inner_net = icon.TwoStepRegistration(inner_net, icon.FunctionFromVectorField(networks.tallUNet2(dimension=dimension)))
+
+    net = GradientICONSparse(inner_net, loss_fn, lmbda=lmbda)
+    net.assign_identity_map(input_shape)
+    return net
+
+
+def get_unigradicon():
+    net = make_network(input_shape, include_last_step=True)
+    from os.path import exists
+    weights_location = "network_weights/unigradicon1.0/Step_2_final.trch"
+    if not exists(weights_location):
+        print("Downloading pretrained model")
+        import urllib.request
+        import os
+        download_path = "https://github.com/uncbiag/uniGradICON/releases/download/unigradicon_weights/Step_2_final.trch"
+        os.makedirs("network_weights/unigradicon1.0/", exist_ok=True)
+        urllib.request.urlretrieve(download_path, weights_location)
+    trained_weights = torch.load(weights_location, map_location=torch.device("cpu"))
+    net.regis_net.load_state_dict(trained_weights)
+    net.to(config.device)
+    net.eval()
+    return net
+
+def preprocess(image):
+    image = itk.CastImageFilter[type(image), itk.Image[itk.F, 3]].New()(image)
+    max_ = np.max(np.array(image))
+    image = itk.shift_scale_image_filter(image, shift=0., scale = .9 / max_)
+    
+    return image
+
+def main():
+    import itk
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--fixed")
+    parser.add_argument("--moving")
+    parser.add_argument("--transform_out")
+    parser.add_argument("--warped_moving_out", default=None)
+
+    args = parser.parse_args()
+
+    net = get_unigradicon()
+
+    fixed = itk.imread(args.fixed)
+    moving = itk.imread(args.moving)
+
+    phi_AB, phi_BA = icon_registration.itk_wrapper.register_pair(net,preprocess(moving), preprocess(fixed), finetune_steps=50)
+
+    itk.transformwrite([phi_AB], args.transform_out)
+
+    if args.warped_moving_out:
+        interpolator = itk.LinearInterpolateImageFunction.New(moving)
+        warped_image_A = itk.resample_image_filter(
+                moving,
+                transform=phi_AB,
+                interpolator=interpolator,
+                use_reference_image=True,
+                reference_image=fixed
+                )
+        itk.imwrite(warped_image_A, args.warped_moving_out)
+
+
+
+
+
