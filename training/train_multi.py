f78e27ff698cbeb7c2a30eb3842050b1422103dd
Basar Demir <bdemir@biag-gpu6.cs.unc.edu>
2024-10-28 02:30:37 -0400

    Add the multigradicon training scripts.

--- training/train_multi.py
+++ training/train_multi.py
@@ -5,38 +5,85 @@ from datetime import datetime
 from tqdm import tqdm
 import torch
 import torch.nn.functional as F
-from dataset import COPDDataset, HCPDataset, OAIDataset, L2rAbdomenDataset
+import dataset_multi
 from torch.utils.data import ConcatDataset, DataLoader
-
-import icon_registration as icon
-import icon_registration.networks as networks
 from icon_registration.losses import ICONLoss, to_floats
-
 from unigradicon import make_network
+import math
 
 def write_stats(writer, stats: ICONLoss, ite, prefix=""):
     for k, v in to_floats(stats)._asdict().items():
         writer.add_scalar(f"{prefix}{k}", v, ite)
 
 input_shape = [1, 1, 175, 175, 175]
-
+DATA_NUM = 4000
 BATCH_SIZE= 4
 device_ids = [1, 0, 2, 3]
 GPUS = len(device_ids)
-EXP_DIR = "./results/unigradicon/"
+EXP_DIR = "./results/multigradicon/"
 
-def get_dataset():
-    data_num = 1000
-    return ConcatDataset(
-        (
-        COPDDataset(desired_shape=input_shape[2:], device=device_ids[0], ROI_only=True, data_num=data_num),
-        OAIDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=data_num),
-        HCPDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=data_num),
-        L2rAbdomenDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=data_num)
-        )
-    )
+def get_multi_training_set():
+    randomization = 'random'
+    
+    datasets = [
+        dataset_multi.COPDDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, ROI_only=False),
+        dataset_multi.BratsRegDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, randomization=randomization),
+        dataset_multi.L2rAbdomenDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, randomization=randomization, augmentation = True),
+        dataset_multi.HCPDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, randomization=randomization),
+        dataset_multi.ABCDFAMDDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, randomization=randomization),
+        dataset_multi.OAIMMDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True),
+        dataset_multi.L2rMRCTDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, augmentation = True),
+        dataset_multi.UKBiobankDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, randomization=randomization),
+    ]
+    
+    dataset =  ConcatDataset(datasets)
+    
+    #modality based weighting
+    dataset_weights = []
+    for dataset in datasets:
+        weight = [dataset.region_num * math.comb(len(dataset.modalities) + 1, 2)] * len(dataset)
+        dataset_weights.append(weight)
+    
+    return dataset, dataset_weights
 
-def augment(image_A, image_B):
+def get_multi_finetuning_set():
+    randomization = 'random'
+    
+    datasets = [
+        dataset_multi.PancreasDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True),
+        dataset_multi.L2rThoraxCBCTDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True),
+        dataset_multi.ABCDDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True),
+        dataset_multi.COPDDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, ROI_only=True),
+        dataset_multi.BratsRegDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, randomization=randomization),
+        dataset_multi.L2rAbdomenDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, randomization=randomization, augmentation = False),
+        dataset_multi.HCPDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, randomization=randomization),
+        dataset_multi.OAIMMDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True),
+        dataset_multi.L2rMRCTDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, augmentation = False),
+        dataset_multi.UKBiobankDataset(desired_shape=input_shape[2:], device=device_ids[0], data_num=DATA_NUM, return_labels=True, randomization=randomization),
+    ]
+    
+    dataset =  ConcatDataset(datasets)
+    
+    #anatomic region based weighting
+    anatomies_count = {}
+    for dataset in datasets:
+        for anatomy in dataset.anatomies:
+            if anatomy not in anatomies_count:
+                anatomies_count[anatomy] = 0
+            anatomies_count[anatomy] += 1
+            
+    max_anatomy = max(anatomies_count.values())
+    
+    dataset_weights = []
+    for dataset in datasets:
+        weight = 0
+        for anatomy in dataset.anatomies:
+            weight += max_anatomy / anatomies_count[anatomy]
+        dataset_weights.append([weight] * len(dataset))
+    
+    return dataset, dataset_weights
+
+def augment(image_A, image_B, label_A, label_B):
     device = image_A.device
     identity_list = []
     for i in range(image_A.shape[0]):
@@ -66,6 +113,7 @@ def augment(image_A, image_B):
         warped_A = torch.cat([warped_A, warped_A_seg], axis=1)
     else:
         warped_A = F.grid_sample(image_A, forward_grid, padding_mode='border')
+        warped_label_A = F.grid_sample(label_A, forward_grid, padding_mode='border')
 
     noise = torch.randn((image_A.shape[0], 3, 4), device=device)
     forward = identity + .05 * noise  
@@ -81,12 +129,13 @@ def augment(image_A, image_B):
         warped_B = torch.cat([warped_B, warped_B_seg], axis=1)
     else:
         warped_B = F.grid_sample(image_B, forward_grid, padding_mode='border')
+        warped_label_B = F.grid_sample(label_B, forward_grid, padding_mode='border')
 
-    return warped_A, warped_B
+    return warped_A, warped_B, warped_label_A, warped_label_B
 
-def train_kernel(optimizer, net, moving_image, fixed_image, writer, ite):
+def train_kernel(optimizer, net, moving_image, fixed_image, moving_label, fixed_label, writer, ite):
     optimizer.zero_grad()
-    loss_object = net(moving_image, fixed_image)
+    loss_object = net(moving_image, fixed_image, moving_label, fixed_label)
     loss = torch.mean(loss_object.all_loss)
     loss.backward()
     optimizer.step()
@@ -122,13 +171,12 @@ def train(
 
     iteration = 0
     for epoch in tqdm(range(epochs)):
-        for moving_image, fixed_image in data_loader:
-            moving_image, fixed_image = moving_image.cuda(), fixed_image.cuda()
+        for moving_image, fixed_image, moving_label, fixed_label in data_loader:
+            moving_image, fixed_image, moving_label, fixed_label = moving_image.cuda(), fixed_image.cuda(), moving_label.cuda(), fixed_label.cuda()
             if data_augmenter is not None:
                 with torch.no_grad():
-                    moving_image, fixed_image = data_augmenter(moving_image, fixed_image)
-            train_kernel(optimizer, net, moving_image, fixed_image,
-                         writer, iteration)
+                    moving_image, fixed_image, moving_image, fixed_image = data_augmenter(moving_image, fixed_image, moving_label, fixed_label)
+            train_kernel(optimizer, net, moving_image, fixed_image, moving_image, fixed_image, writer, iteration)
             iteration += 1
 
             step_callback(unwrapped_net)
@@ -145,7 +193,7 @@ def train(
             )
 
         if epoch % eval_period == 0:
-            visualization_moving, visualization_fixed = next(iter(val_data_loader))
+            visualization_moving, visualization_fixed, _, _ = next(iter(val_data_loader))
             visualization_moving, visualization_fixed = visualization_moving[:, :1].cuda(), visualization_fixed[:, :1].cuda()
             unwrapped_net.eval()
             warped = []
@@ -196,7 +244,7 @@ def train(
 
 def train_two_stage(input_shape, data_loader, val_data_loader, GPUS, epochs, eval_period, save_period, resume_from):
 
-    net = make_network(input_shape, include_last_step=False)
+    net = make_network(input_shape, include_last_step=False, use_label=True)
 
     torch.cuda.set_device(device_ids[0])
     torch.backends.cudnn.enabled = True
@@ -263,6 +311,27 @@ def train_two_stage(input_shape, data_loader, val_data_loader, GPUS, epochs, eva
                 net_2.regis_net.state_dict(),
                 footsteps.output_dir + "checkpoints/Step_2_final.trch",
             )
+    
+    return net_2
+    
+def finetune(net, data_loader, val_data_loader, GPUS, epochs, eval_period, save_period):
+    if GPUS == 1:
+        net_par = net.cuda()
+    else:
+        net_par = torch.nn.DataParallel(net, device_ids=device_ids, output_device=device_ids[0]).cuda()
+    optimizer = torch.optim.Adam(net_par.parameters(), lr=0.00005)
+    
+    net_par.train()
+    
+    footsteps.output_dir_impl = footsteps.output_dir + "finetune/"
+    os.makedirs(footsteps.output_dir + "checkpoints", exist_ok=True)
+
+    train(net_par, optimizer, data_loader, val_data_loader, unwrapped_net=net, epochs=epochs, eval_period=eval_period, save_period=save_period, data_augmenter=augment)
+    
+    torch.save(
+                net.regis_net.state_dict(),
+                footsteps.output_dir + "checkpoints/Step_2_final.trch",
+            )
 
 if __name__ == "__main__":
     import argparse
@@ -275,14 +344,16 @@ if __name__ == "__main__":
     import footsteps
     footsteps.initialize(output_root=EXP_DIR)
 
-    dataset = get_dataset()
+    dataset, weights = get_multi_training_set()
+    
     dataloader = DataLoader(
         dataset,
         batch_size=BATCH_SIZE*GPUS,
-        shuffle=True,
         num_workers=4,
         drop_last=True,
+        sampler=torch.utils.data.WeightedRandomSampler(weights, DATA_NUM)
     )
+    
     val_dataloader = DataLoader(
         dataset,
         batch_size=BATCH_SIZE,
@@ -294,4 +365,29 @@ if __name__ == "__main__":
 
     os.makedirs(footsteps.output_dir + "checkpoints", exist_ok=True)
 
-    train_two_stage(input_shape, dataloader, val_dataloader, GPUS, [801,201], 20, 20, resume_from)
+    print("Start training...")
+    net = train_two_stage(input_shape, dataloader, val_dataloader, GPUS, [801,201], 20, 20, resume_from)
+    
+    del dataloader, val_dataloader, dataset, weights
+    
+    print("Start finetuning...")
+    
+    fine_dataset, fine_weights = get_multi_finetuning_set()
+    
+    fine_dataloader = DataLoader(
+        fine_dataset,
+        batch_size=BATCH_SIZE*GPUS,
+        num_workers=4,
+        drop_last=True,
+        sampler=torch.utils.data.WeightedRandomSampler(fine_weights, DATA_NUM)
+    )
+    
+    fine_val_dataloader = DataLoader(
+        fine_dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+        num_workers=4,
+        drop_last=True,
+    )
+    
+    finetune(net, fine_dataloader, fine_val_dataloader, GPUS, 100, 20, 20)
